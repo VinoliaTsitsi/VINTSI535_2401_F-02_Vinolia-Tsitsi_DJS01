333d13fe9cf06598af766887f644ddc32e1e867b
VinoliaTsitsi <tsitsivinolia07@gmail.com>
2024-05-09 15:36:30 +0200

    updated the code

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const fuel = 5000; // remaining fuel (kg)
 const fuelBurningRate = 0.5; // fuel burn rate (kg/s)
 
 
-const finalDistance = initialDistance + (initialVelocity * (initialTime / 3600) ) //calcultes new distance
+const finalDistance = initialDistance + (initialVelocity * (initialTime / 3600) ); //calcultes new distance
+
 const remainingFuel = fuel - (fuelBurningRate * initialTime);  //calculates remaining fuel
 
 
@@ -16,7 +17,7 @@ const props= {
   velocity: initialVelocity,
   accelaration: initialAcceleration,
   time: initialTime
-}
+};
 
 function calcNewVelocity (props) {
   const { acceleration, velocity, time} = props; 
@@ -25,9 +26,8 @@ return velocity + (initialAcceleration * initialTime) * 3.6; //calculates new ve
 const finalVelocity = calcNewVelocity(props); 
 
 if (finalDistance !== 10000 || remainingFuel !== 3200 || finalVelocity !== 48880) {
- console.log("Error: invalid values") } 
+ console.log("Error: invalid values") } ; 
 
-// Pick up an error with how the function below is called and make it robust to such errors
 
 console.log(`Corrected New Velocity: ${finalVelocity.toFixed(2)} km/h`);
 console.log(`Corrected New Distance: ${finalDistance.toFixed(2)} km`);
